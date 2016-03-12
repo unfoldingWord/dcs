dcfe5e0d7f1b522540fe6251b9f783b6e3edbd6c
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-03-12 16:00:19 -0700

    Reverted files back to upstream

--- routers/user/auth.go
+++ routers/user/auth.go
@@ -126,9 +126,9 @@ func SignInPost(ctx *context.Context, form auth.SignInForm) {
 		// This is for our old Dokuwiki users who are using smd5 password hashing. Want them to change password so it is PBRDF2 hasing
 		ctx.Flash.Error("You need to update your password to use the new Door43. Please update your password before proceeding further.")
 		ctx.Redirect(setting.AppSubUrl + "/user/settings/password")
-	} else {
-		ctx.Redirect(setting.AppSubUrl + "/")
+		return
 	}
+	ctx.Redirect(setting.AppSubUrl + "/")
 }
 
 func SignOut(ctx *context.Context) {
