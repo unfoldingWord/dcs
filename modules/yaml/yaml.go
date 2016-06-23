ee31ae0fd82b1547476d6e6f30693ba61c260f10
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-06-23 22:21:00 +0000

    Added YAML viewer

--- modules/yaml/yaml.go
+++ modules/yaml/yaml.go
@@ -8,43 +8,137 @@ import (
 	"fmt"
 	"strings"
 	"reflect"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/gogits/gogs/modules/log"
 )
 
-func renderHtmlTable(m yaml.MapSlice) string {
-	var thead, tbody string
+var Sanitizer = bluemonday.UGCPolicy()
+
+// IsYamlFile reports whether name looks like a Yaml file
+// based on its extension.
+func IsYamlFile(name string) bool {
+	name = strings.ToLower(name)
+	if ".yaml" == filepath.Ext(name) {
+		return true
+	}
+	return false
+}
+
+func renderHorizontalHtmlTable(m yaml.MapSlice) string {
+	var thead, tbody, table string
 	var mi yaml.MapItem
 	for _, mi = range m {
 		key := mi.Key
 		value := mi.Value
 
 		if  key != nil && reflect.TypeOf(key).String() == "yaml.MapSlice" {
-			key = renderHtmlTable(key.(yaml.MapSlice))
+			key = renderHorizontalHtmlTable(key.(yaml.MapSlice))
 		}
 		thead += fmt.Sprintf("<th>%v</th>", key)
 
 		if value != nil && reflect.TypeOf(value).String() == "yaml.MapSlice" {
-			value = renderHtmlTable(value.(yaml.MapSlice))
+			value = renderHorizontalHtmlTable(value.(yaml.MapSlice))
 		}
 		tbody += fmt.Sprintf("<td>%v</td>", value)
 	}
 
-	table := ""
+	table = ""
 	if len(thead) > 0 {
 		table = fmt.Sprintf(`<table data="yaml-metadata"><thead><tr>%s</tr></thead><tbody><tr>%s</tr></table>`, thead, tbody)
 	}
 	return table
 }
 
-func RenderYamlHtmlTable(data []byte) []byte {
-	m := yaml.MapSlice{}
+func renderVerticalHtmlTable(m []yaml.MapSlice) string {
+	var ms yaml.MapSlice
+	var mi yaml.MapItem
+	var table string
+
+	for _, ms = range m {
+		table += `<table data="yaml-metadata">`
+		for _, mi = range ms {
+			key := mi.Key
+			value := mi.Value
+
+			table += `<tr>`
+			if key != nil && reflect.TypeOf(key).String() == "yaml.MapSlice" {
+				key = renderHorizontalHtmlTable(key.(yaml.MapSlice))
+			} else if key != nil && reflect.TypeOf(key).String() == "[]interface {}" {
+				var ks string
+				for _, ki := range key.([]interface {}) {
+					log.Info("KI: %v", ki)
+					log.Info("Type: %s", reflect.TypeOf(ki).String())
+					ks += renderHorizontalHtmlTable(ki.(yaml.MapSlice))
+				}
+				key = ks
+			}
+			table += fmt.Sprintf("<td>%v</td>", key)
+
+			if value != nil && reflect.TypeOf(value).String() == "yaml.MapSlice" {
+				value = renderHorizontalHtmlTable(value.(yaml.MapSlice))
+			} else if value != nil && reflect.TypeOf(value).String() == "[]interface {}" {
+				value = value.([]interface{})
+				v := make([]yaml.MapSlice, len(value.([]interface{})))
+				for i, vs := range value.([]interface{}) {
+					v[i] = vs.(yaml.MapSlice)
+				}
+				value = renderVerticalHtmlTable(v)
+			}
+			table += fmt.Sprintf("<td>%v</td>", value)
+
+			table += `</tr>`
+		}
+		table += "</table>"
+	}
+
+	return table
+}
+
+func RenderYaml(data []byte) []byte {
+	m := []yaml.MapSlice{}
+
+	if len(data) < 1 {
+		return []byte("")
+	}
+
+	lines := strings.Split(string(data), "\r\n")
+	if len(lines) == 1 {
+		lines = strings.Split(string(data), "\n")
+	}
+	if len(lines) < 1 {
+		return []byte("")
+	}
 
 	if err := yaml.Unmarshal(data, &m); err != nil {
 		return []byte("")
 	}
 
-	return []byte(renderHtmlTable(m))
+	return []byte(renderVerticalHtmlTable(m))
+}
+
+func RenderMarkdownYaml(data []byte) []byte {
+	m := yaml.MapSlice{}
+
+	if len(data) < 1 {
+		return []byte("")
+	}
+
+	lines := strings.Split(string(data), "\r\n")
+	if len(lines) == 1 {
+		lines = strings.Split(string(data), "\n")
+	}
+	if len(lines) < 1 || lines[0] != "---" {
+		return []byte("")
+	}
+
+	if err := yaml.Unmarshal(data, &m); err != nil {
+		return []byte("")
+	}
+
+	return []byte(renderHorizontalHtmlTable(m))
 }
 
 func StripYamlFromText(data []byte) []byte {
@@ -75,16 +169,13 @@ func StripYamlFromText(data []byte) []byte {
 	return []byte(body)
 }
 
-// RenderString renders any YAML section (top of file, denoted by --- on first line and end of YAML)
-// into an HTML table and appends ready of the text after
 func Render(rawBytes []byte) []byte {
-	htmlTable := RenderYamlHtmlTable(rawBytes)
-	body := StripYamlFromText(rawBytes)
-	return append(htmlTable, body...)
+	result := RenderYaml(rawBytes)
+	result = Sanitizer.SanitizeBytes(result)
+	return result
 }
 
 // Renders the YAML and text as a string
 func RenderString(rawBytes []byte) string {
 	return string(Render(rawBytes))
 }
-
