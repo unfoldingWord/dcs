eebb42e9da38ba88e2a9d7bbe724f48e29bc74eb
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2016-12-01 11:54:13 -0500

    Make a page to list all tags

--- models/hashtag.go
+++ models/hashtag.go
@@ -6,9 +6,10 @@ package models
 
 import (
 	"time"
+	"github.com/go-xorm/xorm"
 )
 
-// Webhook represents a web hook object.
+// Hashtag represents a hashtag object.
 type Hashtag struct {
 	ID           int64 `xorm:"pk autoincr"`
 	UserID       int64
@@ -18,6 +19,42 @@ type Hashtag struct {
 	FilePath     string `xorm:"TEXT NOT NULL"`
 	CreatedUnix  int64
 	UpdatedUnix  int64
+}
+
+// Gets a summary of the hashtags for repositories with the specified prefix.
+// The `engine` parameter is for unit testing.
+// If `engine` is nil, then the `x` engine from models.go is used.
+func GetHashtagSummary(engine *xorm.Engine, repoPrefix string) ([]map[string]string, error) {
+
+	sql := `SELECT h.tag_name, COUNT(*) AS count_of_occurrences
+FROM repository AS r INNER JOIN hashtag AS h ON r.id = h.repo_id
+WHERE r.name LIKE ?
+GROUP BY h.tag_name
+ORDER BY LOWER(h.tag_name)`
+
+	// if engine is nil, use the `x` engine from models.go
+	if engine == nil {
+		engine = x
+	}
+
+	// get the requested list of tags
+	results, err := engine.Query(sql, repoPrefix + "%")
+
+	if err != nil {
+		return nil, err
+	}
+
+	// convert the byte arrays returned by the Query function into regular strings
+	returnVal := make([]map[string]string, len(results))
+	for idx, row := range results {
+		values := make(map[string]string)
+		for key, value := range row {
+			values[key] = string(value)
+		}
+		returnVal[idx] = values
+	}
+
+	return returnVal, nil
 }
 
 func (h *Hashtag) BeforeInsert() {
