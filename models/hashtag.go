9334f4c3d7c3006f00fcc48de4d0f906381ffc11
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-02-28 10:33:15 -0700

    Disambinguation Pages

--- models/hashtag.go
+++ models/hashtag.go
@@ -6,19 +6,56 @@ package models
 
 import (
 	"time"
+	"fmt"
 	"github.com/go-xorm/xorm"
 )
 
 // Hashtag represents a hashtag object.
 type Hashtag struct {
-	ID           int64 `xorm:"pk autoincr"`
-	UserID       int64
-	RepoID       int64
-	Lang         string `xorm:"TEXT NOT NULL"`
-	TagName      string `xorm:"TEXT NOT NULL"`
-	FilePath     string `xorm:"TEXT NOT NULL"`
-	CreatedUnix  int64
-	UpdatedUnix  int64
+	ID          int64 `xorm:"pk autoincr"`
+	UserID      int64
+	User        *User  `xorm:"-"`
+	RepoID      int64
+	Repo        *Repository  `xorm:"-"`
+	Lang        string `xorm:"TEXT NOT NULL"`
+	TagName     string `xorm:"TEXT NOT NULL"`
+	FilePath    string `xorm:"TEXT NOT NULL"`
+	FileURL     string `xorm:"-"`
+	CreatedUnix int64
+	UpdatedUnix int64
+}
+
+// LoadAttributes loads the attribute of this hashtag.
+func (hashtag *Hashtag) LoadAttributes() error {
+	return hashtag.loadAttributes(x)
+}
+
+func (hashtag *Hashtag) loadAttributes(e Engine) (err error) {
+	if hashtag.Repo == nil {
+		hashtag.Repo, err = getRepositoryByID(e, hashtag.RepoID)
+		if err != nil {
+			return fmt.Errorf("getRepositoryByID [%d]: %v", hashtag.RepoID, err)
+		}
+	}
+
+	if hashtag.User == nil {
+		hashtag.User, err = getUserByID(e, hashtag.UserID)
+		if err != nil {
+			hashtag.UserID = -1
+			hashtag.User = NewGhostUser()
+			if !IsErrUserNotExist(err) {
+				return fmt.Errorf("getUserByID.(user) [%d]: %v", hashtag.UserID, err)
+			}
+			err = nil
+			return
+		}
+	}
+
+	if hashtag.FileURL == "" {
+		hashtag.FileURL = hashtag.Repo.HTMLURL() + "/src/master/" + hashtag.FilePath
+	}
+
+	return nil
 }
 
 // GetHashtagSummary gets a summary of the hashtags for repositories with the specified prefix.
@@ -35,7 +72,7 @@ GROUP BY h.tag_name
 ORDER BY LOWER(h.tag_name)`
 
 	// get the requested list of tags
-	results, err := engine.Query(sql, ownerID, repoPrefix + "%")
+	results, err := engine.Query(sql, ownerID, repoPrefix+"%")
 
 	if err != nil {
 		return nil, err
@@ -52,6 +89,33 @@ ORDER BY LOWER(h.tag_name)`
 	}
 
 	return returnVal, nil
+}
+
+// GetHashtags gets all hashtags by the repo prefix, UserID, and TagName
+func GetHashtags(repoPrefix string, userID int64, tagName string) ([]Hashtag, error) {
+	return getHashtags(x, repoPrefix, userID, tagName)
+}
+
+func getHashtags(engine *xorm.Engine, repoPrefix string, userID int64, tagName string) ([]Hashtag, error) {
+	hashtags := []Hashtag{}
+	if err := engine.
+		Join("INNER", "repository", "hashtag.repo_id = repository.id").
+		Where("tag_name = ?", tagName).
+		And("user_id = ?", userID).
+		And("repository.lower_name LIKE ?", repoPrefix+"%").
+		And("repository.lower_name LIKE ?", repoPrefix+"%").
+		Asc("file_path").
+		Find(&hashtags); err != nil {
+		return nil, err;
+	}
+
+	for i := range hashtags {
+		if err := hashtags[i].LoadAttributes(); err != nil {
+			return nil, fmt.Errorf("LoadAttributes [%d]: %v", hashtags[i].ID, err)
+		}
+	}
+
+	return hashtags, nil
 }
 
 func (h *Hashtag) BeforeInsert() {
--- routers/repo/hashtag.go
+++ routers/repo/hashtag.go
@@ -1,20 +1,50 @@
 package repo
 
 import (
-	"code.gitea.io/gitea/modules/context"
-	"code.gitea.io/gitea/modules/base"
 	"strings"
-	"code.gitea.io/gitea/models"
-	"code.gitea.io/gitea/modules/log"
 	"net/http"
+	"code.gitea.io/gitea/models"
+	"code.gitea.io/gitea/modules/base"
+	"code.gitea.io/gitea/modules/context"
+	"code.gitea.io/gitea/modules/log"
 )
 
 const (
 	HASHTAGS    base.TplName = "repo/hashtag/list"
+	HASHTAG     base.TplName = "repo/hashtag/page"
 )
 
 // Hashtags produces a page listing all hashtags for this language, with counts
 func Hashtags(ctx *context.Context) {
+	// get the LANG-ubn repository name prefix
+	var repo_prefix string
+	if strings.HasSuffix(ctx.Repo.Repository.Name, "-ubn") {
+		repo_prefix = ctx.Repo.Repository.Name
+	} else {
+		char_index := strings.LastIndex(ctx.Repo.Repository.Name, "-ubn-")
+		repo_prefix = ctx.Repo.Repository.Name[0 : char_index+4]
+	}
+
+	ctx.Data["username"] = ctx.Repo.Repository.Owner.Name
+	ctx.Data["reponame"] = ctx.Repo.Repository.Name
+	ctx.Data["RepoLink"] = ctx.Repo.Repository.Link()
+	ctx.Data["Title"] = ctx.Tr("repo.hashtag.all_hashtags", ctx.Repo.Repository.Owner.Name+"/"+repo_prefix)
+	ctx.Data["PageIsHashtags"] = true
+
+	if results, err := models.GetHashtagSummary(repo_prefix, ctx.Repo.Repository.Owner.ID); err != nil {
+		log.Error(4, "GetHashtagSummary(%s, %s): %v", repo_prefix, ctx.Repo.Repository.Owner.ID, err)
+		ctx.Handle(http.StatusInternalServerError, "GetHashtagSummary", err)
+		return
+	} else {
+		ctx.Data["Tags"] = results
+	}
+
+	ctx.HTML(200, HASHTAGS)
+}
+
+// HashtagDisambiguation produces a disambiguation page
+func HashtagDisambiguation(ctx *context.Context) {
+	hashtag := ctx.Params(":hashtag")
 
 	// get the LANG-ubn repository name prefix
 	var repo_prefix string
@@ -22,28 +52,24 @@ func Hashtags(ctx *context.Context) {
 		repo_prefix = ctx.Repo.Repository.Name
 	} else {
 		char_index := strings.LastIndex(ctx.Repo.Repository.Name, "-ubn-")
-		repo_prefix = ctx.Repo.Repository.Name[0:char_index + 4]
+		repo_prefix = ctx.Repo.Repository.Name[0 : char_index+4]
 	}
 
+	ctx.Data["Repo"] = ctx.Repo.Repository
 	ctx.Data["username"] = ctx.Repo.Repository.Owner.Name
 	ctx.Data["reponame"] = ctx.Repo.Repository.Name
 	ctx.Data["RepoLink"] = ctx.Repo.Repository.Link()
-	ctx.Data["Title"] = ctx.Tr("repo.hashtag.all_hashtags", ctx.Repo.Repository.Owner.Name + "/" + repo_prefix)
-	results, err := models.GetHashtagSummary(repo_prefix, ctx.Repo.Repository.Owner.ID)
+	ctx.Data["Title"] = ctx.Tr("repo.hashtag.hashtag_title", "#"+hashtag, repo_prefix)
+	ctx.Data["Hashtag"] = hashtag
+	ctx.Data["PageIsHashtags"] = true
 
-	if err != nil {
-		log.Error(4, "Hashtags: %v", err)
-		ctx.Handle(http.StatusInternalServerError, "GetHashtagSummary", err)
+	if results, err := models.GetHashtags(repo_prefix, ctx.Repo.Repository.Owner.ID, hashtag); err != nil {
+		log.Error(4, "GetHashtagPages(%s, %s, %s): %v", repo_prefix, ctx.Repo.Repository.Owner.ID, hashtag, err)
+		ctx.Handle(http.StatusInternalServerError, "GetHashtagPages", err)
 		return
+	} else {
+		ctx.Data["Tags"] = results
 	}
-	ctx.Data["Tags"] = results
 
-	ctx.HTML(200, HASHTAGS)
-}
-
-// HashtagDisambiguation produces a disambiguation page
-func HashtagDisambiguation(ctx *context.Context) {
-
-	hashtag := ctx.Params(":hashtag")
-	print(hashtag)
+	ctx.HTML(200, HASHTAG)
 }
