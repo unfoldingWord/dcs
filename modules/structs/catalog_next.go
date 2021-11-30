19bbe79b8fc3071ac0413ef0e68de94553875aa3
Richard Mahn <richmahn@users.noreply.github.com>
2021-11-30 13:24:44 -0500

    Adds new files

---
+++ modules/structs/catalog_next.go
@@ -0,0 +1,139 @@
+// Copyright 2020 unfoldingWord. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package structs
+
+import "time"
+
+// CatalogV3 represents the root of the v3 Catalog
+type CatalogV3 struct {
+	Catalogs    []map[string]string  `json:"catalogs"`
+	Languages   []*CatalogV3Language `json:"languages"`
+	LastUpdated time.Time            `json:"last_updated"`
+}
+
+// CatalogV3Language represents a language in the catalog v3 languages array
+type CatalogV3Language struct {
+	Identifier  string               `json:"identifier"`
+	Title       string               `json:"title"`
+	Direction   string               `json:"direction"`
+	Resources   []*CatalogV3Resource `json:"resources"`
+	LastUpdated time.Time            `json:"last_updated"`
+}
+
+// CatalogV3Resource represents a resource in the catalog v3 resources array
+type CatalogV3Resource struct {
+	Identifier  string        `json:"identifier"`
+	Title       string        `json:"title"`
+	Subject     string        `json:"subject"`
+	Version     string        `json:"version"`
+	Checking    *interface{}  `json:"checking"`
+	Comment     *string       `json:"comment"`
+	Contributor *interface{}  `json:"contributor"`
+	Creator     string        `json:"creator"`
+	Description string        `json:"description"`
+	Formats     *interface{}  `json:"formats"`
+	Issued      time.Time     `json:"issued"`
+	Modified    time.Time     `json:"modified"`
+	Projects    *interface{}  `json:"projects"`
+	Publisher   string        `json:"publisher"`
+	Relation    *interface{}  `json:"relation"`
+	Rights      string        `json:"rights"`
+	Source      []interface{} `json:"source"`
+}
+
+// CatalogV4 represents a repository's metadata of a tag or default branch
+type CatalogV4 struct {
+	ID                     int64         `json:"id"`
+	Self                   string        `json:"url"`
+	Repo                   string        `json:"repo"`
+	Owner                  string        `json:"owner"`
+	RepoURL                string        `json:"repo_url"`
+	ReleaseURL             string        `json:"release_url"`
+	TarballURL             string        `json:"tarbar_url"`
+	ZipballURL             string        `json:"zipball_url"`
+	Language               string        `json:"lang_code"`
+	Subject                string        `json:"subject"`
+	Title                  string        `json:"title"`
+	BranchOrTag            string        `json:"branch_or_tag_name"`
+	Stage                  string        `json:"stage"`
+	MetadataURL            string        `json:"metadata_url"`
+	MetadataJSONURL        string        `json:"metadata_json_url"`
+	MetadataAPIContentsURL string        `json:"metadata_api_contents_url"`
+	MetadataVersion        string        `json:"metadata_version"`
+	Released               time.Time     `json:"released"`
+	Books                  []string      `json:"books"`
+	Ingredients            []interface{} `json:"ingredients,omitempty"`
+}
+
+// CatalogV5 represents a repository's metadata of a tag or default branch for V5
+type CatalogV5 struct {
+	ID                     int64         `json:"id"`
+	Self                   string        `json:"url"`
+	Name                   string        `json:"name"`
+	Owner                  string        `json:"owner"`
+	FullName               string        `json:"full_name"`
+	Repo                   *Repository   `json:"repo"`
+	Release                *Release      `json:"release"`
+	TarballURL             string        `json:"tarbar_url"`
+	ZipballURL             string        `json:"zipball_url"`
+	GitTreesURL            string        `json:"git_trees_url"`
+	ContentsURL            string        `json:"contents_url"`
+	Language               string        `json:"language"`
+	Subject                string        `json:"subject"`
+	Title                  string        `json:"title"`
+	BranchOrTag            string        `json:"branch_or_tag_name"`
+	Stage                  string        `json:"stage"`
+	MetadataURL            string        `json:"metadata_url"`
+	MetadataJSONURL        string        `json:"metadata_json_url"`
+	MetadataAPIContentsURL string        `json:"metadata_api_contents_url"`
+	MetadataVersion        string        `json:"metadata_version"`
+	Released               time.Time     `json:"released"`
+	Books                  []string      `json:"books"`
+	Ingredients            []interface{} `json:"ingredients,omitempty"`
+}
+
+// CatalogSearchResultsV4 results of a successful search for V4
+type CatalogSearchResultsV4 struct {
+	OK   bool         `json:"ok"`
+	Data []*CatalogV4 `json:"data"`
+}
+
+// CatalogSearchResultsV5 results of a successful search for V5
+type CatalogSearchResultsV5 struct {
+	OK          bool         `json:"ok"`
+	Data        []*CatalogV5 `json:"data"`
+	LastUpdated time.Time    `json:"last_updated"`
+}
+
+// CatalogVersionEndpoints Info on the versions of the catalog
+type CatalogVersionEndpoints struct {
+	Latest   string            `json:"latest"`
+	Versions map[string]string `json:"versions"`
+}
+
+// CatalogVersionEndpointsResponse response with the endpoints for all versions of the catalog
+type CatalogVersionEndpointsResponse struct {
+	OK   bool                       `json:"ok"`
+	Data []*CatalogVersionEndpoints `json:"data"`
+}
+
+// CatalogStages a repo's catalog stages
+type CatalogStages struct {
+	Production    *CatalogStage `json:"prod"`
+	PreProduction *CatalogStage `json:"preprod"`
+	Draft         *CatalogStage `json:"draft"`
+	Latest        *CatalogStage `json:"latest"`
+}
+
+// CatalogStage a repo's catalog stage metadata
+type CatalogStage struct {
+	Tag         string    `json:"branch_or_tag_name"`
+	ReleaseURL  *string   `json:"release_url"`
+	Released    time.Time `json:"released"`
+	ZipballURL  string    `json:"zipball_url"`
+	TarballURL  string    `json:"tarball_url"`
+	GitTreesURL string    `json:"git_trees_url"`
+	ContentsURL string    `json:"contents_url"`
+}
---
+++ routers/api/catalog/v3/api.go
@@ -0,0 +1,230 @@
+// Copyright 2021 unfoldingWord. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package v5 Catalog Next v5 API.
+//
+// This documentation describes the DCS Catalog Next v5 API.
+//
+//     Schemes: http, https
+//     BasePath: /api/catalog/v5
+//     Version: 4.0.1
+//     License: MIT http://opensource.org/licenses/MIT
+//
+//     Consumes:
+//     - application/json
+//     - text/plain
+//
+//     Produces:
+//     - application/json
+//     - text/html
+//
+//     Security:
+//     - BasicAuth :
+//     - Token :
+//     - AccessToken :
+//     - AuthorizationHeaderToken :
+//     - SudoParam :
+//     - SudoHeader :
+//     - TOTPHeader :
+//
+//     SecurityDefinitions:
+//     BasicAuth:
+//          type: basic
+//     Token:
+//          type: apiKey
+//          name: token
+//          in: query
+//     AccessToken:
+//          type: apiKey
+//          name: access_token
+//          in: query
+//     AuthorizationHeaderToken:
+//          type: apiKey
+//          name: Authorization
+//          in: header
+//          description: API tokens must be prepended with "token" followed by a space.
+//     SudoParam:
+//          type: apiKey
+//          name: sudo
+//          in: query
+//          description: Sudo API request as the user provided as the key. Admin privileges are required.
+//     SudoHeader:
+//          type: apiKey
+//          name: Sudo
+//          in: header
+//          description: Sudo API request as the user provided as the key. Admin privileges are required.
+//     TOTPHeader:
+//          type: apiKey
+//          name: X-GITEA-OTP
+//          in: header
+//          description: Must be used in combination with BasicAuth if two-factor authentication is enabled.
+//
+// swagger:meta
+package v3
+
+import (
+	"net/http"
+	"strings"
+
+	"code.gitea.io/gitea/models"
+	"code.gitea.io/gitea/modules/context"
+	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/modules/web"
+	_ "code.gitea.io/gitea/routers/api/v1/swagger" // for swagger generation
+
+	"gitea.com/go-chi/session"
+	"github.com/go-chi/cors"
+)
+
+func sudo() func(ctx *context.APIContext) {
+	return func(ctx *context.APIContext) {
+		sudo := ctx.Query("sudo")
+		if len(sudo) == 0 {
+			sudo = ctx.Req.Header.Get("Sudo")
+		}
+
+		if len(sudo) > 0 {
+			if ctx.IsSigned && ctx.User.IsAdmin {
+				user, err := models.GetUserByName(sudo)
+				if err != nil {
+					if models.IsErrUserNotExist(err) {
+						ctx.NotFound()
+					} else {
+						ctx.Error(http.StatusInternalServerError, "GetUserByName", err)
+					}
+					return
+				}
+				log.Trace("Sudo from (%s) to: %s", ctx.User.Name, user.Name)
+				ctx.User = user
+			} else {
+				ctx.JSON(http.StatusForbidden, map[string]string{
+					"message": "Only administrators allowed to sudo.",
+				})
+				return
+			}
+		}
+	}
+}
+
+func repoAssignment() func(ctx *context.APIContext) {
+	return func(ctx *context.APIContext) {
+		userName := ctx.Params("username")
+		repoName := ctx.Params("reponame")
+
+		var (
+			owner *models.User
+			err   error
+		)
+
+		// Check if the user is the same as the repository owner.
+		if ctx.IsSigned && ctx.User.LowerName == strings.ToLower(userName) {
+			owner = ctx.User
+		} else {
+			owner, err = models.GetUserByName(userName)
+			if err != nil {
+				if models.IsErrUserNotExist(err) {
+					if redirectUserID, err := models.LookupUserRedirect(userName); err == nil {
+						context.RedirectToUser(ctx.Context, userName, redirectUserID)
+					} else if models.IsErrUserRedirectNotExist(err) {
+						ctx.NotFound("GetUserByName", err)
+					} else {
+						ctx.Error(http.StatusInternalServerError, "LookupUserRedirect", err)
+					}
+				} else {
+					ctx.Error(http.StatusInternalServerError, "GetUserByName", err)
+				}
+				return
+			}
+		}
+		ctx.Repo.Owner = owner
+
+		// Get repository.
+		repo, err := models.GetRepositoryByName(owner.ID, repoName)
+		if err != nil {
+			if models.IsErrRepoNotExist(err) {
+				redirectRepoID, err := models.LookupRepoRedirect(owner.ID, repoName)
+				if err == nil {
+					context.RedirectToRepo(ctx.Context, redirectRepoID)
+				} else if models.IsErrRepoRedirectNotExist(err) {
+					ctx.NotFound()
+				} else {
+					ctx.Error(http.StatusInternalServerError, "LookupRepoRedirect", err)
+				}
+			} else {
+				ctx.Error(http.StatusInternalServerError, "GetRepositoryByName", err)
+			}
+			return
+		}
+
+		repo.Owner = owner
+		ctx.Repo.Repository = repo
+
+		ctx.Repo.Permission, err = models.GetUserRepoPermission(repo, ctx.User)
+		if err != nil {
+			ctx.Error(http.StatusInternalServerError, "GetUserRepoPermission", err)
+			return
+		}
+
+		if !ctx.Repo.HasAccess() {
+			ctx.NotFound()
+			return
+		}
+	}
+}
+
+// Routes registers all catalog v5 APIs routes to web application.
+func Routes() *web.Route {
+	var m = web.NewRoute()
+
+	m.Use(session.Sessioner(session.Options{
+		Provider:       setting.SessionConfig.Provider,
+		ProviderConfig: setting.SessionConfig.ProviderConfig,
+		CookieName:     setting.SessionConfig.CookieName,
+		CookiePath:     setting.SessionConfig.CookiePath,
+		Gclifetime:     setting.SessionConfig.Gclifetime,
+		Maxlifetime:    setting.SessionConfig.Maxlifetime,
+		Secure:         setting.SessionConfig.Secure,
+		Domain:         setting.SessionConfig.Domain,
+	}))
+	m.Use(securityHeaders())
+	if setting.CORSConfig.Enabled {
+		m.Use(cors.Handler(cors.Options{
+			//Scheme:           setting.CORSConfig.Scheme, // FIXME: the cors middleware needs scheme option
+			AllowedOrigins: setting.CORSConfig.AllowDomain,
+			//setting.CORSConfig.AllowSubdomain // FIXME: the cors middleware needs allowSubdomain option
+			AllowedMethods:   setting.CORSConfig.Methods,
+			AllowCredentials: setting.CORSConfig.AllowCredentials,
+			MaxAge:           int(setting.CORSConfig.MaxAge.Seconds()),
+		}))
+	}
+	m.Use(context.APIContexter())
+	m.Use(context.ToggleAPI(&context.ToggleOptions{
+		SignInRequired: setting.Service.RequireSignInView,
+	}))
+
+	m.Group("", func() {
+		// Miscellaneous
+		if setting.API.EnableSwagger {
+			m.Get("/swagger", func(ctx *context.APIContext) {
+				ctx.Redirect("../swagger")
+			})
+		}
+
+		m.Get("", CatalogV3)
+	}, sudo())
+
+	return m
+}
+
+func securityHeaders() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+			// CORB: https://www.chromium.org/Home/chromium-security/corb-for-developers
+			// http://stackoverflow.com/a/3146618/244009
+			resp.Header().Set("x-content-type-options", "nosniff")
+			next.ServeHTTP(resp, req)
+		})
+	}
+}
