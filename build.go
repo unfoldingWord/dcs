245b19701f390f421636ead94df5c50b38e08e48
Richard Mahn <richmahn@users.noreply.github.com>
2022-02-15 04:40:59 -0700

    Changes vet

--- build.go
+++ build.go
@@ -18,7 +18,7 @@ import (
 	_ "golang.org/x/tools/cover"
 
 	// for vet
-	_ "gitea.com/unfoldingword/gitea-vet"
+	_ "code.gitea.io/gitea-vet"
 
 	// for swagger
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
