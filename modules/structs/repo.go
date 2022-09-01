d6fe50a714e496ca762184c0c646ca722d6c40f2
Richard Mahn <richmahn@users.noreply.github.com>
2022-09-01 09:19:18 -0600

    Fixes comments for swagger

--- modules/structs/repo.go
+++ modules/structs/repo.go
@@ -59,7 +59,6 @@ type Repository struct {
 	Parent        *Repository `json:"parent"`
 	Mirror        bool        `json:"mirror"`
 	Size          int         `json:"size"`
-	Language      string      `json:"language"`
 	LanguagesURL  string      `json:"languages_url"`
 	HTMLURL       string      `json:"html_url"`
 	SSHURL        string      `json:"ssh_url"`
@@ -98,17 +97,26 @@ type Repository struct {
 	// swagger:strfmt date-time
 	MirrorUpdated time.Time     `json:"mirror_updated,omitempty"`
 	RepoTransfer  *RepoTransfer `json:"repo_transfer"`
-	/*** DCS Customizations ***/
-	LanguageTitle   string                 `json:"language_title"`
-	LanguageDir     string                 `json:"language_direction"`
-	LanguageIsGL    bool                   `json:"language_is_gl"`
-	Subject         string                 `json:"subject"`
-	Books           []string               `json:"books,omitempty"`
-	AlignmentCounts map[string]interface{} `json:"alignment_counts,omitempty"`
-	Title           string                 `json:"title"`
-	CheckingLevel   string                 `json:"checking_level"`
-	Catalog         *CatalogStages         `json:"catalog"`
-	/*** END DCS Customizations ***/
+	// language from the manifest
+	Language string `json:"language"` // DCS Customiations
+	// language title from the manifest
+	LanguageTitle string `json:"language_title"` // DCS Customiations
+	// language direction from the manifest
+	LanguageDir string `json:"language_direction"` // DCS Customiations
+	// is the language a GL
+	LanguageIsGL bool `json:"language_is_gl"` // DCS Customiations
+	// subject from the manifest
+	Subject string `json:"subject"` // DCS Customiations
+	// project IDs from the manifest
+	Books []string `json:"books,omitempty"` // DCS Customiations
+	// the count of alignments of each book
+	AlignmentCounts map[string]interface{} `json:"alignment_counts,omitempty"` // DCS Customiations
+	// title from the manifest
+	Title string `json:"title"` // DCS Customiations
+	// checking level from the manifest
+	CheckingLevel string `json:"checking_level"` // DCS Customiations
+	// latest catalog entry of each stage
+	Catalog *CatalogStages `json:"catalog"` // DCS Customiations
 }
 
 // CreateRepoOption options when creating repository
