416e4a318ebb105908eaea017d40f3ba9d70ea3f
Richard Mahn <richmahn@users.noreply.github.com>
2022-10-03 17:02:49 -0600

    Fixes changes to upstream

--- models/repo/repo_list.go
+++ models/repo/repo_list.go
@@ -15,6 +15,7 @@ import (
 	"code.gitea.io/gitea/models/unit"
 	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/container"
+	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/structs"
 	"code.gitea.io/gitea/modules/util"
 
