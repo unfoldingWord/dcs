fc45b61983fc69c5cde5fddb4b2c9c34dec2005c
Richard Mahn <richmahn@users.noreply.github.com>
2022-10-08 14:35:56 -0600

    Updates tests

--- models/organization/org_test.go
+++ models/organization/org_test.go
@@ -322,8 +322,8 @@ func TestAccessibleReposEnv_RepoIDs(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, expectedRepoIDs, repoIDs)
 	}
-	testSuccess(2, 1, 100, []int64{3, 5, 32})
-	testSuccess(4, 0, 100, []int64{3, 32})
+	testSuccess(2, 1, 100, []int64{3})
+	testSuccess(4, 0, 100, []int64{32})
 }
 
 func TestAccessibleReposEnv_Repos(t *testing.T) {
