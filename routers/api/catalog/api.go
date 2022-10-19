3c83272684c7518879ba5f6c40297493365e026a
Richard Mahn <richmahn@users.noreply.github.com>
2022-10-19 16:20:27 -0600

    Fixes linting

--- routers/api/catalog/api.go
+++ routers/api/catalog/api.go
@@ -6,84 +6,13 @@ package catalog
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
-	access_model "code.gitea.io/gitea/models/perm/access"
-	repo_model "code.gitea.io/gitea/models/repo"
-	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/context"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/modules/web"
 
 	"github.com/go-chi/cors"
 )
-
-func repoAssignment() func(ctx *context.APIContext) {
-	return func(ctx *context.APIContext) {
-		userName := ctx.Params("username")
-		repoName := ctx.Params("reponame")
-
-		var (
-			owner *user_model.User
-			err   error
-		)
-
-		// Check if the user is the same as the repository owner.
-		if ctx.IsSigned && ctx.Doer.LowerName == strings.ToLower(userName) {
-			owner = ctx.Doer
-		} else {
-			owner, err = user_model.GetUserByName(ctx, userName)
-			if err != nil {
-				if user_model.IsErrUserNotExist(err) {
-					if redirectUserID, err := user_model.LookupUserRedirect(userName); err == nil {
-						context.RedirectToUser(ctx.Context, userName, redirectUserID)
-					} else if user_model.IsErrUserRedirectNotExist(err) {
-						ctx.NotFound("GetUserByName", err)
-					} else {
-						ctx.Error(http.StatusInternalServerError, "LookupUserRedirect", err)
-					}
-				} else {
-					ctx.Error(http.StatusInternalServerError, "GetUserByName", err)
-				}
-				return
-			}
-		}
-		ctx.Repo.Owner = owner
-		ctx.ContextUser = owner
-
-		// Get repository.
-		repo, err := repo_model.GetRepositoryByName(owner.ID, repoName)
-		if err != nil {
-			if repo_model.IsErrRepoNotExist(err) {
-				redirectRepoID, err := repo_model.LookupRedirect(owner.ID, repoName)
-				if err == nil {
-					context.RedirectToRepo(ctx.Context, redirectRepoID)
-				} else if repo_model.IsErrRedirectNotExist(err) {
-					ctx.NotFound()
-				} else {
-					ctx.Error(http.StatusInternalServerError, "LookupRepoRedirect", err)
-				}
-			} else {
-				ctx.Error(http.StatusInternalServerError, "GetRepositoryByName", err)
-			}
-			return
-		}
-
-		repo.Owner = owner
-		ctx.Repo.Repository = repo
-
-		ctx.Repo.Permission, err = access_model.GetUserRepoPermission(ctx, repo, ctx.Doer)
-		if err != nil {
-			ctx.Error(http.StatusInternalServerError, "GetUserRepoPermission", err)
-			return
-		}
-
-		if !ctx.Repo.HasAccess() {
-			ctx.NotFound()
-			return
-		}
-	}
-}
 
 // Routes registers all v1 APIs routes to web application.
 func Routes() *web.Route {
