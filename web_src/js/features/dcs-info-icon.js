725425e19170389703136fe32d230285cd878886
Richard Mahn <richmahn@users.noreply.github.com>
2022-10-27 18:21:06 -0600

    Changes popup() to use tippy()

--- web_src/js/features/dcs-info-icon.js
+++ web_src/js/features/dcs-info-icon.js
@@ -1,5 +1,17 @@
-import $ from 'jquery';
+import {createTippy} from '../modules/tippy.js';
 
 export function initDCSInfoIcon() {
-  $('#dcs-info-icon').popup();
+  const icon = document.getElementById('dcs-info-icon');
+  if (!icon) {
+    return;
+  }
+  const tooltip = document.getElementById('dcs-info-icon-tooltip');
+  if (!tooltip) {
+    return;
+  }
+  createTippy(icon, {
+    allowHTML: true,
+    maxWidth: 650,
+    content: tooltip.innerHTML,
+  });
 }
