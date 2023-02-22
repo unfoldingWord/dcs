ed7e7bb68659d6a725bbbfdbedbef646923004a5
Richard Mahn <richmahn@users.noreply.github.com>
2023-02-22 12:59:59 -0700

    Makes structs non-pointers

--- modules/base/door43.go
+++ modules/base/door43.go
@@ -372,11 +372,11 @@ type TcTsManifest struct {
 		Name      string `json:"name"`
 		Direction string `json:"direction"`
 	} `json:"target_language"`
-	Type *struct {
+	Type struct {
 		ID   string `json:"id"`
 		Name string `json:"name"`
 	} `json:"type"`
-	Resource *struct {
+	Resource struct {
 		ID   string `json:"id"`
 		Name string `json:"name"`
 	} `json:"resource"`
