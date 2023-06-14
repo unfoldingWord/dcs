f7ef9ef511a5dec79d88b1f4da00a863878de0af
Richard Mahn <richmahn@users.noreply.github.com>
2023-06-14 03:03:22 -0600

    Fixes swagger

--- routers/api/v1/catalog/catalog.go
+++ routers/api/v1/catalog/catalog.go
@@ -584,7 +584,7 @@ func ListCatalogLanguages(ctx *context.APIContext) {
 
 // GetCatalogEntry Get the catalog entry from the given ownername, reponame and ref
 func GetCatalogEntry(ctx *context.APIContext) {
-	// swagger:operation GET /catalog/entry/{owner}/{repo}/{tag} catalog catalogGetEntry
+	// swagger:operation GET /catalog/entry/{owner}/{repo}/{ref} catalog catalogGetEntry
 	// ---
 	// summary: Catalog entry
 	// produces:
