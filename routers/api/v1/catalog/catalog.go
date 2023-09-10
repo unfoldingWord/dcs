ed01f565b89120a876297fd0f61a72c71c01ffdd
Richard Mahn <rich.mahn@unfoldingword.org>
2023-09-10 04:15:33 -0700

    Fixes merge conflict

--- routers/api/v1/catalog/catalog.go
+++ routers/api/v1/catalog/catalog.go
@@ -766,7 +766,7 @@ func GetCatalogEntry(ctx *context.APIContext) {
 		ctx.Error(http.StatusInternalServerError, "GetUserRepoPermission", err)
 		return
 	}
-	ctx.JSON(http.StatusOK, convert.ToCatalogEntry(ctx, dm, perm.AccessMode))
+	ctx.JSON(http.StatusOK, convert.ToCatalogEntry(ctx, dm, perm))
 }
 
 // GetCatalogMetadata Get the metadata (RC 0.2 manifest) in JSON format for the given ownername, reponame and ref
@@ -940,7 +940,7 @@ func searchCatalog(ctx *context.APIContext) {
 		if err != nil {
 			ctx.Error(http.StatusInternalServerError, "GetUserRepoPermissions", err)
 		}
-		dmAPI := convert.ToCatalogEntry(ctx, dm, perm.AccessMode)
+		dmAPI := convert.ToCatalogEntry(ctx, dm, perm)
 		if opts.ShowIngredients == util.OptionalBoolFalse {
 			dmAPI.Ingredients = nil
 		}
