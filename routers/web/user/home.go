649569339f296b20c3d3f17d9c4f507e895c3c14
Lunny Xiao <xiaolunwen@gmail.com>
2024-05-30 15:04:01 +0800

    Use repo as of renderctx's member rather than a repoPath on metas (#29222)
    Use a `gitrepo.Repository` in the markup's RenderContext but not store
    the repository's path.

--- routers/web/user/home.go
+++ routers/web/user/home.go
@@ -269,6 +269,7 @@ func Milestones(ctx *context.Context) {
 			},
 			Metas: milestones[i].Repo.ComposeMetas(ctx),
 			Ctx:   ctx,
+			Repo:  milestones[i].Repo,
 		}, milestones[i].Content)
 		if err != nil {
 			ctx.ServerError("RenderString", err)
