17fdbfd8e866377f29f4786be399864a155e6bb5
techknowlogick <techknowlogick@gitea.com>
2024-09-09 22:23:07 -0400

    bump to go 1.23 (#31855)

--- models/repo/repo_list.go
+++ models/repo/repo_list.go
@@ -802,7 +802,7 @@ func GetUserRepositories(ctx context.Context, opts *SearchRepoOptions) (Reposito
 		cond = cond.And(builder.Eq{"is_private": false})
 	}
 
-	if opts.LowerNames != nil && len(opts.LowerNames) > 0 {
+	if len(opts.LowerNames) > 0 {
 		cond = cond.And(builder.In("`repository`.lower_name", opts.LowerNames))
 	}
 
