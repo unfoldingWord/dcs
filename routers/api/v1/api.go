7029d16037b12acf096966755122b7cee0e5fbb7
Zettat123 <zettat123@gmail.com>
2024-10-22 13:09:19 +0800

    Add `DISABLE_ORGANIZATIONS_PAGE` and `DISABLE_CODE_PAGE` settings for explore pages and fix an issue related to user search (#32288)
    These settings can allow users to only display the repositories explore page.
    
    Thanks to yp05327 and wxiaoguang !
    
    ---------
    
    Co-authored-by: Giteabot <teabot@gitea.io>
    Co-authored-by: wxiaoguang <wxiaoguang@gmail.com>

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -358,11 +358,19 @@ func reqToken() func(ctx *context.APIContext) {
 
 func reqExploreSignIn() func(ctx *context.APIContext) {
 	return func(ctx *context.APIContext) {
-		if setting.Service.Explore.RequireSigninView && !ctx.IsSigned {
+		if (setting.Service.RequireSignInView || setting.Service.Explore.RequireSigninView) && !ctx.IsSigned {
 			/*** DCS Customizations ***/
 			return
-			//ctx.Error(http.StatusUnauthorized, "reqExploreSignIn", "you must be signed in to search for users")
+			// ctx.Error(http.StatusUnauthorized, "reqExploreSignIn", "you must be signed in to search for users")
 			/*** END DCS Customizations ***/
+		}
+	}
+}
+
+func reqUsersExploreEnabled() func(ctx *context.APIContext) {
+	return func(ctx *context.APIContext) {
+		if setting.Service.Explore.DisableUsersPage {
+			ctx.NotFound()
 		}
 	}
 }
@@ -960,7 +968,7 @@ func Routes() *web.Router {
 
 		// Users (requires user scope)
 		m.Group("/users", func() {
-			m.Get("/search", reqExploreSignIn(), user.Search)
+			m.Get("/search", reqExploreSignIn(), reqUsersExploreEnabled(), user.Search)
 
 			m.Group("/{username}", func() {
 				m.Get("", reqExploreSignIn(), user.GetInfo)
