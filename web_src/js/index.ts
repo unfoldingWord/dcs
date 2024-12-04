6a51fd5441dbcffb92f0266267a9531a725932a0
wxiaoguang <wxiaoguang@gmail.com>
2024-12-04 10:11:34 +0800

    Refactor markdown editor and use it for milestone description editor (#32688)
    Refactor markdown editor to clarify its "preview" behavior and remove
    jQuery code.
    
    Close #15045
    
    ---------
    
    Co-authored-by: silverwind <me@silverwind.io>

--- web_src/js/index.ts
+++ web_src/js/index.ts
@@ -88,7 +88,11 @@ import {
   initGlobalButtons,
   initGlobalDeleteButton,
 } from './features/common-button.ts';
-import {initGlobalEnterQuickSubmit, initGlobalFormDirtyLeaveConfirm} from './features/common-form.ts';
+import {
+  initGlobalComboMarkdownEditor,
+  initGlobalEnterQuickSubmit,
+  initGlobalFormDirtyLeaveConfirm,
+} from './features/common-form.ts';
 
 initGiteaFomantic();
 initDirAuto();
@@ -132,6 +136,7 @@ onDomReady(() => {
     initGlobalCopyToClipboardListener,
     initGlobalEnterQuickSubmit,
     initGlobalFormDirtyLeaveConfirm,
+    initGlobalComboMarkdownEditor,
     initGlobalDeleteButton,
 
     initCommonOrganization,
