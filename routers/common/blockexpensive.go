17ca278a8fc7e5d82e5703ac92472b1137132678
Richard Mahn <richmahn@users.noreply.github.com>
2025-06-18 09:10:19 -0600

    Revert "Update blockexpensive.go"

--- routers/common/blockexpensive.go
+++ routers/common/blockexpensive.go
@@ -48,8 +48,6 @@ func isRoutePathExpensive(routePattern string) bool {
 		"/{username}/{reponame}/media/",
 		// "/{username}/{reponame}/raw/", // DCS Customizations
 		// "/{username}/{reponame}/src/", // DCS Customizations
-		"/{username}/{reponame}/src/commit/", // DCS Customizations
-		"/{username}/{reponame}/compare/",    // DCS Customizations
 
 		// issue & PR related (no trailing slash)
 		"/{username}/{reponame}/issues",
