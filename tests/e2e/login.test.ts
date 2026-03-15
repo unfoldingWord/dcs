1e574a5e0db02a75de4b444c8c93ad9e6c171ece
Richard Mahn <rich.mahn@unfoldingword.org>
2026-03-14 22:41:02 -0400

    Fixes logo for DCS

--- tests/e2e/login.test.ts
+++ tests/e2e/login.test.ts
@@ -3,7 +3,7 @@ import {login, logout} from './utils.ts';
 
 test('homepage', async ({page}) => {
   await page.goto('/');
-  await expect(page.getByRole('img', {name: 'Logo'})).toHaveAttribute('src', '/assets/img/logo.svg');
+  await expect(page.locator('#navbar-logo img')).toHaveAttribute('src', '/assets/img/logo.svg');
 });
 
 test('login and logout', async ({page}) => {
